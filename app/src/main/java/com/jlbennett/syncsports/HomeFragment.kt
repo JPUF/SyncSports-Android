d80df468e77dedb22989bda0de093b825c6f94ec
psyjb12 <psyjb12@nottingham.ac.uk>
2019-10-11 15:05:20 +0100

    Initial commit

---
+++ app/src/main/java/com/jlbennett/syncsports/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.jlbennett.syncsports
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jlbennett.syncsports.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false)
+
+        return binding.root
+    }
+
+
+}
