cb627829203c4f478ab034efb9acda6df8d16cc0
psyjb12 <psyjb12@nottingham.ac.uk>
2019-10-11 16:04:37 +0100

    Chat Fragment

---
+++ app/src/main/java/com/jlbennett/syncsports/ChatFragment.kt
@@ -0,0 +1,26 @@
+package com.jlbennett.syncsports
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jlbennett.syncsports.databinding.FragmentChatBinding
+
+
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentChatBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_chat, container, false)
+
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/jlbennett/syncsports/SyncFragment.kt
+++ app/src/main/java/com/jlbennett/syncsports/SyncFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.jlbennett.syncsports.databinding.FragmentSyncBinding
 
 
@@ -20,6 +21,9 @@ class SyncFragment : Fragment() {
         val binding: FragmentSyncBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sync, container, false)
 
+        binding.chatButton.setOnClickListener {
+            findNavController().navigate(R.id.action_syncFragment_to_chatFragment)
+        }
         return binding.root
     }
 
