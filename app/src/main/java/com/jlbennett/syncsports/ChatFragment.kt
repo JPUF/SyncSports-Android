ad731a19b68d03e955a29755b4813d396b325fde
JPUF <2009bennettj@gmail.com>
2019-10-29 13:38:44 +0000

    Initial integration with Express server via Socket.io

--- app/src/main/java/com/jlbennett/syncsports/ChatFragment.kt
+++ app/src/main/java/com/jlbennett/syncsports/ChatFragment.kt
@@ -11,9 +11,16 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jlbennett.syncsports.databinding.FragmentChatBinding
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONObject
+import java.util.*
 
 
 class ChatFragment : Fragment() {
+
+    private val socket = IO.socket("http://192.168.122.1:4000")
+    private lateinit var recyclerViewAdapter: ChatMessageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,32 +30,69 @@ class ChatFragment : Fragment() {
             inflater, R.layout.fragment_chat, container, false
         )
 
+        connectToChatAPI()
+
         val args: ChatFragmentArgs by navArgs()
         val matchTime = args.matchTime
-
-        Log.i("chatTime", "MATCH ${matchTime.state} -- ${matchTime.minutes}:${matchTime.seconds}")
         val matchTimeString = "MATCH ${matchTime.state} -- ${matchTime.minutes}:${matchTime.seconds}"
-
         binding.timeText.text = matchTimeString
-        binding.chatMessageList.layoutManager = LinearLayoutManager(this.context)
+        binding.chatMessageList.layoutManager = LinearLayoutManager(this.context)//Set RecyclerView LayoutManager
 
         val dummyMessages = mutableListOf<String>()
         for (i in 1..32) {
             dummyMessages.add("Message: $i")
         }
 
-        val recyclerViewAdapter = ChatMessageAdapter(dummyMessages)
+        recyclerViewAdapter = ChatMessageAdapter(dummyMessages)
         binding.chatMessageList.adapter = recyclerViewAdapter
 
         binding.sendButton.setOnClickListener {
             val chatMessage = binding.inputText.text
             Log.i("chatMessage", "SENDING MESSAGE: $chatMessage")
-            recyclerViewAdapter.addMessage(chatMessage.toString())
             binding.chatMessageList.scrollToPosition(recyclerViewAdapter.itemCount - 1)
             binding.inputText.setText(R.string.empty)
+
+            sendMessage(chatMessage.toString())
         }
 
         return binding.root
+    }
+
+    private fun connectToChatAPI() {
+        Log.d("ChatNetworkLog", "connectToChatAPI Called")
+        socket.on(Socket.EVENT_CONNECT) {
+            val usernameString = "AndroidApp"
+            socket.emit("username", usernameString)
+        }
+
+
+        socket.on("chat_message") { args ->
+            val msgObject = args[0] as JSONObject
+            val username: String = msgObject.get("username") as String
+            val message: String = msgObject.get("message") as String
+
+            val formattedString = "$username: $message"
+            displayMessage(formattedString)
+        }
+
+        socket.connect()
+    }
+
+    private fun sendMessage(message: String) {
+        Log.d("ChatNetworkLog", "sendMessage Called")
+        val msgObject = JSONObject()
+        msgObject.put("username", "AndroidApp")
+        msgObject.put("message", message)
+        msgObject.put("user_time", Date().time)
+        socket.emit("chat_message", msgObject)
+    }
+
+    private fun displayMessage(message: String) {
+        Log.d("ChatNetworkLog", "Message Received: $message")
+        activity!!.runOnUiThread {
+            //Ugly
+            recyclerViewAdapter.addMessage(message)
+        }
     }
 
 }
