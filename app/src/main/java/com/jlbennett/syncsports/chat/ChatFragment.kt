cd01d5fb1b7a280b44f43e5a8b3b6b90925a23e7
psyjb12 <psyjb12@nottingham.ac.uk>
2019-11-04 13:41:18 +0000

    Moved network functionality to ViewModel (untested)

--- app/src/main/java/com/jlbennett/syncsports/chat/ChatFragment.kt
+++ app/src/main/java/com/jlbennett/syncsports/chat/ChatFragment.kt
@@ -3,27 +3,23 @@ package com.jlbennett.syncsports.chat
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jlbennett.syncsports.R
 import com.jlbennett.syncsports.databinding.FragmentChatBinding
 import com.jlbennett.syncsports.util.User
-import io.socket.client.IO
-import io.socket.client.Socket
-import org.json.JSONObject
-import java.util.*
 
 
 class ChatFragment : Fragment() {
 
-    private val socket = IO.socket("http://192.168.122.1:4000")
+
     private lateinit var viewModel: ChatViewModel
     private lateinit var recyclerViewAdapter: ChatMessageAdapter
 
@@ -37,14 +33,22 @@ class ChatFragment : Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(ChatViewModel::class.java)
 
-        connectToChatAPI()
+        viewModel.eventMessageToShow.observe(this, Observer {hasMessageToShow ->
+            if (hasMessageToShow) {
+                displayMessage(ChatMessage(User("mes", Color.RED), "Test Message"))
+                viewModel.onDisplayMessageComplete()
+                //TODO test to see if test message is displayed.
+            }
+        })
+
+        viewModel.connectToChatAPI()
 
         val args: ChatFragmentArgs by navArgs()
         val matchTime = args.matchTime
         val matchTimeString = "MATCH ${matchTime.state} -- ${matchTime.minutes}:${matchTime.seconds}"
         binding.timeText.text = matchTimeString
         binding.chatMessageList.layoutManager = LinearLayoutManager(this.context)//Set RecyclerView LayoutManager
-        
+
         recyclerViewAdapter = ChatMessageAdapter(viewModel.dummyMessages)
         binding.chatMessageList.adapter = recyclerViewAdapter
 
@@ -53,43 +57,12 @@ class ChatFragment : Fragment() {
             binding.chatMessageList.scrollToPosition(recyclerViewAdapter.itemCount - 1)
             binding.inputText.setText(R.string.empty)
 
-            sendMessage(chatMessage.toString())
+            viewModel.sendMessage(chatMessage.toString())
         }
-
         return binding.root
     }
 
-    private fun connectToChatAPI() {
-        Log.d("ChatNetworkLog", "connectToChatAPI Called")
-        socket.on(Socket.EVENT_CONNECT) {
-            val usernameString = "AndroidApp"
-            socket.emit("username", usernameString)
-        }
-
-
-        socket.on("chat_message") { args ->
-            val msgObject = args[0] as JSONObject
-            val username: String = msgObject.get("username") as String
-            val usercolor: String = msgObject.get("color") as String
-            val message: String = msgObject.get("message") as String
-            Log.d("usercolour", "$username - $usercolor - $message")
-            displayMessage(User(username, Color.parseColor(usercolor)), message)
-        }
-
-        socket.connect()
-    }
-
-    private fun sendMessage(message: String) {
-        val msgObject = JSONObject()
-        msgObject.put("username", "AndroidApp")
-        msgObject.put("color", "#5e0104")
-        msgObject.put("message", message)
-        msgObject.put("user_time", Date().time)
-        socket.emit("chat_message", msgObject)
-    }
-
-    private fun displayMessage(user: User, message: String) {
-        val chatMessage = ChatMessage(User(user.name, user.color), message)
+    fun displayMessage(chatMessage: ChatMessage) {
         activity!!.runOnUiThread {
             //Ugly
             recyclerViewAdapter.addMessage(chatMessage)
--- app/src/main/java/com/jlbennett/syncsports/chat/ChatViewModel.kt
+++ app/src/main/java/com/jlbennett/syncsports/chat/ChatViewModel.kt
@@ -2,12 +2,18 @@ package com.jlbennett.syncsports.chat
 
 import android.graphics.Color
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jlbennett.syncsports.util.User
-
-//TODO follow from tut: step 10 onwards. To populate this VM. 
+import io.socket.client.IO
+import io.socket.client.Socket
+import org.json.JSONObject
+import java.util.*
 
 class ChatViewModel : ViewModel() {
+
+    private val socket = IO.socket("http://192.168.122.1:4000")
 
     val dummyMessages = listOf(
         ChatMessage(
@@ -28,12 +34,51 @@ class ChatViewModel : ViewModel() {
         )
     )
 
+    private val _eventMessageToShow = MutableLiveData<Boolean>()
+    val eventMessageToShow: LiveData<Boolean>
+        get() = _eventMessageToShow
+
     init {
         Log.d("viewmodel", "ChatViewModel init")
+        _eventMessageToShow.value = false
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("viewmodel", "ChatViewModel onCleared()")
+    }
+
+    fun connectToChatAPI() {
+        Log.d("ChatNetworkLog", "connectToChatAPI Called")
+        socket.on(Socket.EVENT_CONNECT) {
+            val usernameString = "AndroidApp"
+            socket.emit("username", usernameString)
+        }
+
+
+        socket.on("chat_message") { args ->
+            val msgObject = args[0] as JSONObject
+            val username: String = msgObject.get("username") as String
+            val usercolor: String = msgObject.get("color") as String
+            val message: String = msgObject.get("message") as String
+            Log.d("usercolour", "$username - $usercolor - $message")
+            //displayMessage(User(username, Color.parseColor(usercolor)), message)
+            _eventMessageToShow.value = true
+            //TODO send message with this.
+        }
+        socket.connect()
+    }
+
+    fun sendMessage(message: String) {
+        val msgObject = JSONObject()
+        msgObject.put("username", "AndroidApp")
+        msgObject.put("color", "#5e0104")
+        msgObject.put("message", message)
+        msgObject.put("user_time", Date().time)
+        socket.emit("chat_message", msgObject)
+    }
+
+    fun onDisplayMessageComplete() {
+        _eventMessageToShow.value = false
     }
 }
